ddac1a21dedb947f8ce4b898e9d6188ce1fb2922
Kauã Reidner <kauareidner@hotmail.com>
2023-08-20 14:34:06 -0300

    Initializing a new project. A REST API for Walks and WalksDifficulty

---
+++ NZWalks/Models/Region.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.Models
+{
+    public class Region
+    {
+        public Guid id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        public IEnumerable<Walk> Walks { get; set; }
+        
+    }
+}
+    
---
+++ NZWalks/Models/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.Models
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+
+        public Guid RegionId { get; set; }
+        public Guid WalkDificulty { get; set; }
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
