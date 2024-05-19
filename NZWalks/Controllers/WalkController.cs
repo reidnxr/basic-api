8c4d9167fa38781923c4526197b15375d03f17b1
Kauã Reidner <kauareidner@hotmail.com>
2024-05-19 10:16:49 -0300

    Adding payload validator to WalkController

--- NZWalks/Controllers/WalkController.cs
+++ NZWalks/Controllers/WalkController.cs
@@ -11,11 +11,15 @@ namespace NZWalks.Controllers
     public class WalkController : Controller
     {
         private readonly IMapper _mapper;
+        private readonly IWalkDifficultyService _walkDifficultyService;
+        private readonly IRegionService _regionService;
         private readonly IWalkService _walkService;
 
-        public WalkController(IMapper mapper, IWalkService walkService)
+        public WalkController(IMapper mapper, IWalkService walkService, IWalkDifficultyService walkDifficultyService, IRegionService regionService)
         {
             this._mapper = mapper;
+            this._walkDifficultyService = walkDifficultyService;
+            this._regionService = regionService;
             this._walkService = walkService;
         }
 
@@ -45,6 +49,12 @@ namespace NZWalks.Controllers
         [HttpPost]
         public async Task<IActionResult> Add([FromBody] Models.DTO.AddMethod.Walk walk)
         {
+            bool valid = await IsValid(walk);
+            if (!valid)
+            {
+                return BadRequest(ModelState);
+            }
+
             Walk newWalk = _mapper.Map<Walk>(walk);
             newWalk = await _walkService.Add(newWalk);
 
@@ -57,6 +67,12 @@ namespace NZWalks.Controllers
         [Route("{id:guid}")]
         public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] Models.DTO.UpdateMethod.Walk walk)
         {
+            bool valid = await IsValid(walk);
+            if (!valid)
+            {
+                return BadRequest(ModelState);
+            }
+
             Walk target = _mapper.Map<Walk>(walk);
             target = await _walkService.Update(target, id);
 
@@ -83,12 +99,89 @@ namespace NZWalks.Controllers
         }
 
         #region Private Methods
-        public bool IsValid(Models.DTO.AddMethod.Walk walk)
+
+        public async Task<bool> IsValid(Models.DTO.AddMethod.Walk walk)
         {
             bool valid = true;
 
+            if (walk == null)
+            {
+                ModelState.AddModelError(nameof(walk), $"Walk cannot be null.");
+                valid = false;
+                return valid;
+            }
+
+            if (string.IsNullOrWhiteSpace(walk.Name))
+            {
+                ModelState.AddModelError(nameof(walk.Name), $"{nameof(walk.Name)} cannot be null or empty or white space.");
+            }
+
+            if (walk.Length <= 0)
+            {
+                ModelState.AddModelError(nameof(walk.Length), $"{nameof(walk.Length)} cannot be less than or equal to 0.");
+            }
+            Region region = await _regionService.Get(walk.RegionId);
+            if (region == null)
+            {
+                ModelState.AddModelError(nameof(walk.RegionId), $"{nameof(walk.RegionId)} is invalid.");
+            }
+
+            WalkDifficulty walkDIfficulty = await _walkDifficultyService.Get(walk.WalkDifficultyId);
+            if (walkDIfficulty == null)
+            {
+                ModelState.AddModelError(nameof(walk.WalkDifficultyId), $"{nameof(walk.WalkDifficultyId)} is invalid.");
+            }
+
+
+            if (ModelState.ErrorCount > 0)
+            {
+                valid = false;
+            }
+
             return valid;
         }
+
+        public async Task<bool> IsValid(Models.DTO.UpdateMethod.Walk walk)
+        {
+            bool valid = true;
+
+            if (walk == null)
+            {
+                ModelState.AddModelError(nameof(walk), $"Walk cannot be null.");
+                valid = false;
+                return valid;
+            }
+
+            if (string.IsNullOrWhiteSpace(walk.Name))
+            {
+                ModelState.AddModelError(nameof(walk.Name), $"{nameof(walk.Name)} cannot be null or empty or white space.");
+            }
+
+            if (walk.Length <= 0)
+            {
+                ModelState.AddModelError(nameof(walk.Length), $"{nameof(walk.Length)} cannot be less than or equal to 0.");
+            }
+            Region region = await _regionService.Get(walk.RegionId);
+            if (region == null)
+            {
+                ModelState.AddModelError(nameof(walk.RegionId), $"{nameof(walk.RegionId)} is invalid.");
+            }
+
+            WalkDifficulty walkDIfficulty = await _walkDifficultyService.Get(walk.WalkDifficultyId);
+            if (walkDIfficulty == null)
+            {
+                ModelState.AddModelError(nameof(walk.WalkDifficultyId), $"{nameof(walk.WalkDifficultyId)} is invalid.");
+            }
+
+
+            if (ModelState.ErrorCount > 0)
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+
         #endregion
     }
 }
